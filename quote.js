07b796771cb8a734672c8c4aa4edcad30801c23f
clw12 <clw6444@gmail.com>
2024-07-25 14:12:01 +0100

    Ticket 2b - created function but not solved task

--- quote.js
+++ quote.js
@@ -4,9 +4,28 @@ import { v4 as uuidv4 } from "uuid";
 const fileName = "quotes.json";
 
 export async function getQuotes() {
-//take in quoteText (String)
 
 }
+
+//not take in any arguments
+//read/parse all quote objects from `quotes.json`
+//return an array of all existing quote objects
+// const jsonData = JSON.parse()
+
+import { readFile } from 'node:fs/promises';
+
+async function readFromFile(fileName) {
+    try {
+        const data = await readFile(fileName, 'utf8');
+        //parse the string content as json
+        const quotes = JSON.parse(data)
+        console.log(quotes);
+    } catch (error) {
+        console.error('Error reading file:', error);
+    }
+}
+
+
 
 export async function addQuote(quoteText) {
 
